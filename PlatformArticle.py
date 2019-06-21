8217190f9882c5177601d2d765fb14614466c843
xzkzdx <1968199077@qq.com>
2019-06-21 11:55:13 +0800

    up

---
+++ PlatformArticle.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+# !/usr/bin/python3
+# @Time   : 2019/6/21 11:14
+# @File   : PlatformArticle.py
+
+
+class Article:
+    def __init__(self):
+        pass
+
+
+class History:
+    def __init__(self):
+        pass
+
+
+class Platform:
+    def __init__(self, biz):
+        self.biz = biz
+
+
+class Fiddler:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
